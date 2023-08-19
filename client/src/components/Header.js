83d6d69f365bb16f3652e967864a33b4f9a9ca21
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-08-19 02:53:20 +0200

    c/ converting madzia-portfolio for my portfolio

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,8 @@ import { motion } from 'framer-motion';
 import { SocialIcon } from 'react-social-icons';  
 import { Link } from 'react-router-dom';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 function Header() {
     return (
         <header className="bg-opacity-0 sticky top-0 flex flex-row items-start max-w-7xl mx-auto z-20 xl:items-center p-5">
@@ -22,13 +23,13 @@ function Header() {
             }}
             className="flex flex-row items-center flex-1 mr-auto justify-start"
           >
-            <SocialIcon
-                url="https://www.instagram.com/calu.sfx/"
+            <a href="https://github.com/jakubiwaszkiewicz">
+              <GitHubIcon
                 className="cursor-pointer mx-2"
-                network="instagram"
-                fgColor="white"
-                bgColor="transparent"
-            />
+                style={{ height: 36, width: 36,  }}
+              />
+            </a>
+            
         </motion.div>
         <motion.div
             initial={{
@@ -52,26 +53,24 @@ function Header() {
         </motion.div>
         <motion.div
           initial={{
-              
               opacity: 0,
               scale: 0.5,
-            }}
-            animate={{
+          }}
+          animate={{
               opacity: 1,
               scale: 1,
-            }}
-            transition={{
+          }}
+          transition={{
               duration: 1.5,
-            }}
+          }}
             className="flex flex-row items-center flex-1 ml-auto justify-end"
         >
-            <SocialIcon
-              url="#contact"
+          <a href="https://www.linkedin.com/in/jakub-iwaszkiewicz-635bb4245/">
+            <LinkedInIcon
               className="cursor-pointer mx-2"
-              network="email"
-              fgColor="white"
-              bgColor="transparent"
+              style={{ height: 40, width: 40,  }}
             />
+          </a>
         </motion.div>
       </header>
     );
